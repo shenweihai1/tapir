98fc69827ed3e697a814579956ff42293a3b0775
Ubuntu <azureuser@experiment-client-0.ag0hgbjpqppubcp1dkxzvtxi3f.cx.internal.cloudapp.net>
2021-07-08 14:55:14 +0000

    fix

--- r2.sh
+++ r2.sh
@@ -4,3 +4,8 @@ skill server
 /home/azureuser/tapir/store/tapirstore/server -m txn-l -f /home/azureuser/tapir/store/tools/keys -k 100000 -e 0 -s 0 -c /home/azureuser/tapir/store/tools/shard1.config -i 2 &
 /home/azureuser/tapir/store/tapirstore/server -m txn-l -f /home/azureuser/tapir/store/tools/keys -k 100000 -e 0 -s 0 -c /home/azureuser/tapir/store/tools/shard2.config -i 2 &
 /home/azureuser/tapir/store/tapirstore/server -m txn-l -f /home/azureuser/tapir/store/tools/keys -k 100000 -e 0 -s 0 -c /home/azureuser/tapir/store/tools/shard3.config -i 2 &
+/home/azureuser/tapir/store/tapirstore/server -m txn-l -f /home/azureuser/tapir/store/tools/keys -k 100000 -e 0 -s 0 -c /home/azureuser/tapir/store/tools/shard4.config -i 2 &
+/home/azureuser/tapir/store/tapirstore/server -m txn-l -f /home/azureuser/tapir/store/tools/keys -k 100000 -e 0 -s 0 -c /home/azureuser/tapir/store/tools/shard5.config -i 2 &
+/home/azureuser/tapir/store/tapirstore/server -m txn-l -f /home/azureuser/tapir/store/tools/keys -k 100000 -e 0 -s 0 -c /home/azureuser/tapir/store/tools/shard6.config -i 2 &
+/home/azureuser/tapir/store/tapirstore/server -m txn-l -f /home/azureuser/tapir/store/tools/keys -k 100000 -e 0 -s 0 -c /home/azureuser/tapir/store/tools/shard7.config -i 2 &
+
